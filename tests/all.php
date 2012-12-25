3fafd1cba8837bcf72f27a8a95a31cc0cc91969a
Alvaro Cabrera <pateketrueke@gmail.com>
2012-12-25 14:45:20 -0600

    Some simple testing

---
+++ tests/all.php
@@ -0,0 +1,36 @@
+<?php
+
+$autoload = require dirname(__DIR__).DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'autoload.php';
+
+$data = array(
+    'foo' => 'bar',
+    'candy' => array('does', 'nothing\'s', 'baz', 'buzz'),
+    'else' => 'baz',
+  );
+
+$rules = array(
+    'foo' => array('required'),
+    'candy.1' => array('required','="nothing\'s"'),
+    'else' => array('=candy.2')
+  );
+
+Staple\Validation::setup($rules);
+
+var_dump(Staple\Validation::execute($data));
+
+var_dump(Staple\Validation::errors());
+
+var_dump(Staple\Helpers::parameterize('This world go crazy pa-pawh!'));
+var_dump(Staple\Helpers::underscore('This world go crazy pa-pawh!'));
+var_dump(Staple\Helpers::underscore('ShutUpBro'));
+var_dump(Staple\Helpers::classify('shut-up-bro'));
+var_dump(Staple\Helpers::titlecase('i-am-and-this-is-a-great-thing'));
+
+foreach (array('man', 'fly', 'eye', 'fish', 'words', 'post', 'sheep', 'doctor', 'users', 'account', 'profiles') as $one) {
+  $plural = Staple\Inflector::pluralize($one);
+  $single = Staple\Inflector::singularize($one);
+
+  echo "\n $one => $single : $plural";
+}
+
+echo "\n";
