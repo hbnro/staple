ec35e27ea084a6f60d08082d1e7467427f62738d
Alvaro Cabrera <pateketrueke@gmail.com>
2013-06-07 04:42:17 -0500

    Refactorization

--- lib/Staple/Paginate.php
+++ lib/Staple/Paginate.php
@@ -5,30 +5,36 @@ namespace Staple;
 class Paginate implements \IteratorAggregate
 {
 
-  private $count = 0;
-  private $current = 0;
-
-  private $results = array();
+  private $data = array();
   private $options = array(
               'link_text' => '%d',
               'link_href' => '?p=%d',
               'link_root' => '/',
-              'count_max' => 13,
-              'count_page' => 20,
+              'current' => 0,
+              'per_page' => 20,
+              'num_links' => 10,
+              'total_items' => 0,
             );
+
 
   private function __controller()
   {
   }
 
+  public function __toString()
+  {
+    return join(', ', $this->navlinks());
+  }
+
   public function getIterator()
   {
-    return new \ArrayIterator($this->all());
+    return new \ArrayIterator($this->result());
   }
+
 
   public function __call($method, $arguments)
   {
-    $callback = array($this->results, $method);
+    $callback = array($this->data, $method);
 
     if (is_callable($callback)) {
       return call_user_func_array($callback, $arguments);
@@ -37,14 +43,16 @@ class Paginate implements \IteratorAggregate
     throw new \Exception("Unable to execute '$method' callback");
   }
 
-  public static function build()
+  public static function build($config = NULL)
   {
-    return new static;
+    $obj = new static;
+
+    return $obj->set($config);
   }
 
   public function bind($set)
   {
-    $this->results = $set;
+    $this->data = $set;
 
     return $this;
   }
@@ -61,6 +69,8 @@ class Paginate implements \IteratorAggregate
     } elseif ( $key && ! is_numeric($key)) {
       $this->options[$key] = $value;
     }
+
+    return $this;
   }
 
   public function get($key, $default = FALSE)
@@ -68,57 +78,91 @@ class Paginate implements \IteratorAggregate
     return ! empty($this->options[$key]) ? $this->options[$key] : $default;
   }
 
+  public function result()
+  {
+    $out = $this->data;
+
+    if (is_object($out)) {
+      if ( ! method_exists($out, 'all')) {
+        // TODO: raise exception
+      }
+
+      $out = $out->all();
+    } else {
+      $out = array_slice($out, $this->offset(), $this->per_page());
+    }
+
+    return $out;
+  }
+
   public function total()
   {
-    return (int) $this->count;
+    return (int) $this->get('total_items');
   }
 
   public function pages()
   {
-    return ceil($this->count / $this->get('count_page'));
+    return ceil($this->total() / $this->get('per_page'));
   }
 
-  public function count_page()
+  public function per_page()
   {
-    return (int) $this->get('count_page');
+    return (int) $this->get('per_page');
   }
 
-  public function count_max()
+  public function num_links()
   {
-    return (int) $this->get('count_max');
+    return (int) $this->get('num_links');
   }
 
   public function current()
   {
-    return $this->current ? (int) $this->current : 1;
+    return (int) $this->get('current') ?: 1;
   }
 
-  public function offset($count, $current = FALSE)
+  public function offset()
   {
-    $this->count = (int) $count;
-
-    if ($current !== FALSE) {
-      $this->current = (int) $current;
-    }
-
-    $index = $this->current ? $this->current - 1 : $this->current;
-    $index = floor($index * $this->get('count_page'));
+    $index = $this->current() ? $this->current() - 1 : 0;
+    $index = floor($index * $this->per_page());
 
     return $index;
   }
 
-  public function links($wrap = '[%s]')
+  public function navlinks($wrap = '[%s]')
   {
-    $out = array();
-    $end = $this->pages();
-    $cur = $this->current();
+    $page_num = $this->current();
+    $last_page = $this->pages();
 
-    for ($i = 1; $i <= $end; $i += 1) {
+    $next = $page_num + 1;
+    $show = $this->get('num_links');
+
+    $start = $page_num - $show;
+    $end = $page_num + $show;
+
+
+    if ($page_num == 1)
+    {
+      return $this->links(1, 1, $end, $wrap);
+    }
+
+
+    if ($page_num == $last_page)
+    {
+      return $this->links($last_page, max(1, $start), $last_page, $wrap);
+    }
+
+    return $this->links($page_num, max(1, $start), min($end, $last_page), $wrap);
+  }
+
+  public function links($current, $from, $to, $wrap = '[%s]')
+  {
+    for ($i = $from; $i <= $to; $i += 1) {
       $link = $this->link($i, $this->get('link_text'));
 
-      if ($cur === $i) {
+      if ($current === $i) {
         $link = sprintf($wrap, $link);
       }
+
       $out []= $link;
     }
 
@@ -128,8 +172,9 @@ class Paginate implements \IteratorAggregate
   public function link($num, $text = '', array $args = array())
   {
     $text = $text ? sprintf($this->get('link_text'), number_format($num)) : number_format($num);
+    $html = '<a href="' . $this->url_for($num) . '">' . $text . '</a>';
 
-    return tag('a', $this->url_for($num), $text);
+    return $html;
   }
 
   public function url_for($num)
@@ -145,25 +190,6 @@ class Paginate implements \IteratorAggregate
   public function next_url()
   {
     return $this->current() < $this->pages() ? $this->url_for($this->current() + 1) : FALSE;
-  }
-
-  public function step($from = 0)
-  {
-    $out = 0;
-    $max = $this->count_max();
-    $end = $this->current() + $from;
-
-    for ($i = 0; $i < $end; $i += 1) {
-      if (($i % $max) === 1) {
-        $out += 1;
-      }
-    }
-
-    if ($out > 0) {
-      $out -= 1;
-    }
-
-    return $out;
   }
 
 }
